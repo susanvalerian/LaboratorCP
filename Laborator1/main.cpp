04c24f1cef76b6d3af59261522e78657ab3c38e1
Susan Valerian <susanvalerian@gmail.com>
2017-03-11 21:38:22 +0200

    Exercitiul7

--- Laborator1/main.cpp
+++ Laborator1/main.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main()
 {
-    double cel, fahr;
-    cout << "Temperatura in grade Fahrenheit: ";
-    cin >> fahr;
-    cel = (fahr - 32) * 5./9;
-    cout << "Temperatura in grade Celsius este de " << cel << endl;
+    int v, h;
+    double g;
+    cout << "Inaltimea (cm): ";
+    cin >> h;
+    cout << "Varsta (ani): ";
+    cin >> v;
+    g = 50 + 0.75*(h - 150) + 0.25 * (v - 20);
+    cout << "Greutatea ideala (barbat): " << g << "kg" << endl;
+    cout << "Greutatea ideala (femeie): " << 0.9 * g << "kg" << endl;
     return 0;
 }
